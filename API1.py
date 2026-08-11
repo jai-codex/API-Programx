d81e19ddd74689674986ff92d79a3c99091dcbc6
Shree <khape.jai@gmail.com>
2026-08-11 20:46:50 +0530

    Updated One

--- API1.py
+++ API1.py
@@ -1,12 +1,20 @@
 import requests
 
-username = "kumbharvivek93-cmyk"
+username = input("Enter GitHub username: ")
+
 url = f"https://api.github.com/users/{username}"
+try:
+    response = requests.get(url, timeout=5)
+    if response.status_code == 200:
+        data = response.json()
 
-response = requests.get(url)
-print(response.status_code)
-data = response.json()
-
-print(data["login"])
-print(data["public_repos"])
-print(data["followers"])
+        print("Username:", data["login"])
+        print("Public Repositories:", data["public_repos"])
+        print("Followers:", data["followers"])
+    elif response.status_code == 404:
+        print("User not found!")
+    else:
+        print("Something went wrong.")
+        print("Status Code:", response.status_code)     
+except requests.exceptions.RequestException:
+    print("Network error. Please check your internet connection.")        
